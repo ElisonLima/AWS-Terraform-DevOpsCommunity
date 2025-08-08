f87bcbd53577df7a98b453d98eee99a15efeb7e5
Elison Lima <limagoncalves@live.com>
2025-08-07 21:24:57 -0300

    feat: private subnets and variables

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "Networking CIDR block to be used for the VPC"
+}
+
+variable "project_name" {
+  type        = string
+  description = "project name to be used to name the resources (Name Tag)"
+}
